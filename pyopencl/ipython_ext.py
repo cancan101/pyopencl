867cfc44c2a82ebf580fd2047ef809bc2fdf7fa8
Alex Rothberg <arothberg@4combinator.com>
2014-06-12 14:01:46 -0400

    Created cl_kernel_file magic

--- pyopencl/ipython_ext.py
+++ pyopencl/ipython_ext.py
@@ -1,6 +1,8 @@
 from __future__ import division
 
-from IPython.core.magic import (magics_class, Magics, cell_magic)
+from IPython.core.magic import (magics_class, Magics, cell_magic, line_magic)
+from IPython.core.magics.code import (CodeMagics)
+
 
 import pyopencl as cl
 
@@ -32,6 +34,27 @@ class PyOpenCLMagics(Magics):
         for knl in prg.all_kernels():
             self.shell.user_ns[knl.function_name] = knl
 
+@magics_class
+class PyOpenCLCodeMagics(CodeMagics):
+    @line_magic
+    def cl_kernel_file(self, line):
+        opts,args = self.parse_options(line,'o:f:')
+
+        header = "%%cl_kernel"
+
+        build_options = opts.get('o')
+
+        if build_options:
+            header = "%s %s" % (header, build_options)
+
+        kernel = self.shell.find_user_code(opts.get('f') or args)
+
+        content = "%s\n\n%s" % (header, kernel)
+
+        self.shell.set_next_input(content)
+
 
 def load_ipython_extension(ip):
     ip.register_magics(PyOpenCLMagics)
+    ip.register_magics(PyOpenCLCodeMagics)
+
